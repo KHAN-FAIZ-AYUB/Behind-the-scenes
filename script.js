ae061aa1376287ea0f62f410d1df973641c492ad
Khan Faiz <khan.faiz.ayub@gmail.com>
2023-08-20 01:59:31 +0530

    scooping

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 'use strict';
+
+function calcAge(birthYear) {
+  const age = -birthYear;
+
+  function printAge() {
+    let output = `${firstName}, you are${age},born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true;
+
+      const firstName = 'Steven';
+
+      output = 'NEW OUTPUT';
+
+      const str = `Oh, and youre a millenial, ${firstName}`;
+      console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+    }
+
+    console.log(millenial);
+
+    console.log(output);
+  }
+
+  printAge();
+
+  return age;
+}
+
+const firstName = 'John';
+calcAge(1991);
