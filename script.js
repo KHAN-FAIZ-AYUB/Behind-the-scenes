7f39c8e040439112d50dceca4cf2906e0115464f
Khan Faiz <khan.faiz.ayub@gmail.com>
2023-08-20 16:06:18 +0530

    this keyword

--- script.js
+++ script.js
@@ -77,7 +77,41 @@ function deleteShoppingCart() {
   console.log('All products deleted');
 }
 */
-
+/*
 var x = 1;
 let y = 2;
 const z = 3;
+*/
+
+// console.log(this);
+
+const calcAge = function (birthYear) {
+  console.log(2037 - birthYear);
+  //   console.log(this);
+};
+calcAge(1991);
+
+const calcAgeArrow = birthYear => {
+  console.log(2037 - birthYear);
+  console.log(this);
+};
+calcAge(1981);
+
+const jonas = {
+  year: 1991,
+  calcAge: function () {
+    console.log(this);
+    console.log(2037 - this.year);
+  },
+};
+jonas.calcAge();
+
+const matilda = {
+  year: 2017,
+};
+
+matilda.calcAge = jonas.calcAge;
+matilda.calcAge();
+
+const f = jonas.calcAge;
+f();
