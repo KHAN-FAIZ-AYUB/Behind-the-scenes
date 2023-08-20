b4d0fb9bb6c99da13b14c110ebefebfcd3767323
Khan Faiz <khan.faiz.ayub@gmail.com>
2023-08-20 17:44:15 +0530

    this & argument keywords

--- script.js
+++ script.js
@@ -83,6 +83,7 @@ let y = 2;
 const z = 3;
 */
 
+/*
 // console.log(this);
 
 const calcAge = function (birthYear) {
@@ -115,3 +116,50 @@ matilda.calcAge();
 
 const f = jonas.calcAge;
 f();
+*/
+
+// var firstName='faiz';
+const jonas = {
+  firstName: 'jonas',
+  year: 1998,
+  calcAge: function () {
+    // console.log(this);
+    console.log(2037 - this.year);
+
+    // *****Solution 1*****
+    // const self = this; // self or that
+    // const isMillenial = function () {
+    //   console.log(self);
+    //   console.log(self.year >= 1981 && self.year <= 1996);
+    // };
+
+    // *****Solution 2*****
+    const isMillenial = () => {
+      console.log(this);
+      console.log(this.year >= 1981 && this.year <= 1996);
+    };
+
+    isMillenial();
+  },
+  greet: () => {
+    console.log(this);
+    console.log(`Hey ${this.firstName}`);
+  },
+};
+jonas.greet();
+jonas.calcAge();
+
+// arguments keyword
+const addExpr = function (a, b) {
+  console.log(arguments);
+  return a + b;
+};
+
+addExpr(2, 5);
+addExpr(2, 4, 5, 8, 12);
+
+var addArrow = (a, b) => {
+  console.log(arguments);
+  return a + b;
+};
+addArrow(2, 5, 8);
