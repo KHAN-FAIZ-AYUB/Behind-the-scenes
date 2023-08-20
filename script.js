8c6dd1e72e251d2879625854b957bbedcdc8d06d
Khan Faiz <khan.faiz.ayub@gmail.com>
2023-08-20 13:10:38 +0530

    hosting & TDZ

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 function calcAge(birthYear) {
   const age = -birthYear;
 
@@ -34,3 +35,49 @@ function calcAge(birthYear) {
 
 const firstName = 'John';
 calcAge(1991);
+*/
+
+// variables
+/*
+console.log(me);
+console.log(job);
+console.log(year);
+
+var me = 'Faiz';
+let job = 'Developer';
+const year = 1998;
+*/
+
+// functions
+/*
+console.log(addDecl(2, 3));
+console.log(addExpr(2, 3));
+console.log(addArrow(2, 3));
+
+function addDecl(a, b) {
+  return a + b;
+}
+
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+var addArrow = (a, b) => a + b;
+*/
+
+// Example
+/*
+console.log(numProduct);
+
+if (!numProduct) deleteShoppingCart();
+
+var numProduct = 10;
+
+function deleteShoppingCart() {
+  console.log('All products deleted');
+}
+*/
+
+var x = 1;
+let y = 2;
+const z = 3;
